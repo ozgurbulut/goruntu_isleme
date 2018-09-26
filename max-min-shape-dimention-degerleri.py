f076c3a12ec82352f10c6f7ec7fe5fcac5a2f79d
Özgür Bulut <ozgurbulut0@outlook.com>
2018-09-26 14:06:28 +0300

    Update max-min-shape-dimention-degerleri.py

--- max-min-shape-dimention-degerleri.py
+++ max-min-shape-dimention-degerleri.py
@@ -9,10 +9,16 @@ def my_function_1(img_1):
 
     en_fazla = (img_1[:,:,0].max())
     en_az = (img_1[:,:,0].min())
-    dimention = (img_1.ndim)///eksen_sayisi
-    shape=(img_1.shape)///eksen_degerleri
+    dimention = (img_1.ndim)
+    shape=(img_1.shape)
     print("Dimention degeri =",dimention)
     print("Shape degeri = ",shape)
     print("En buyuk deger =",en_fazla)
     print("En kucuk deger =",en_az)
+    print("En kucuk kirmizi renk degeri:",np.min(img_1[:,:,0]))
+    print("En kucuk kirmizi renk degeri:",np.max(img_1[:,:,0]))
+    print("En kucuk kirmizi renk degeri:",np.min(img_1[:,:,1]))
+    print("En kucuk kirmizi renk degeri:",np.max(img_1[:,:,1]))
+    print("En kucuk kirmizi renk degeri:",np.min(img_1[:,:,2]))
+    print("En kucuk kirmizi renk degeri:",np.max(img_1[:,:,2]))
 my_function_1(img_1)    
