681d7f910e6cd512ea7da678d8b0f714cfe085ec
Özgür Bulut <ozgurbulut0@outlook.com>
2018-09-26 13:56:16 +0300

    Update max-min-shape-dimention-degerleri.py

--- max-min-shape-dimention-degerleri.py
+++ max-min-shape-dimention-degerleri.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-en_fazla = 0
-en_az = 0
-
 img_1=plt.imread("image.jpg")
-en_fazla = (img_1[:,:,0].max())
-en_az = (img_1[:,:,0].min())
-dimention = (img_1.ndim)
-shape=(img_1.shape)
-print("Dimention degeri =",dimention)
-print("Shape degeri = ",shape)
-print("En buyuk deger =",en_fazla)
-print("En kucuk deger =",en_az)
+
+def my_function_1(img_1):
+    
+    en_fazla = 0
+    en_az = 0
+
+    en_fazla = (img_1[:,:,0].max())
+    en_az = (img_1[:,:,0].min())
+    dimention = (img_1.ndim)
+    shape=(img_1.shape)
+    print("Dimention degeri =",dimention)
+    print("Shape degeri = ",shape)
+    print("En buyuk deger =",en_fazla)
+    print("En kucuk deger =",en_az)
+my_function_1(img_1)    
