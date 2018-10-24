2b13aef99a8d23c8783afd5af7b8c30d30ddcf12
Özgür Bulut <ozgurbulut0@outlook.com>
2018-10-24 14:47:11 +0300

    Update karakter_okuma_tc.py

--- karakter_okuma_tc.py
+++ karakter_okuma_tc.py
@@ -1,19 +1,23 @@
-#extorn ve intorn degerlerini tut.
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-img=mpimg.imread("tct.jpg")
+img=mpimg.imread("index.png")
+
+
+# In[19]:
+
+
 def get_distance(v,w=[1/3,1/3,1/3]):
     a,b,c=v[0],v[1],v[2]
     w1,w2,w3=w[0],w[1],w[2]
     d=((a**2)*w1+(b**2)*w2+(c**2)*w3)**.5
-    return d 
-my_RGB=[1,2,3]
-gray_level=get_distance(my_RGB)
-print(gray_level)
-my_RGB=[10,20,3]
-gray_level=get_distance(my_RGB,[.6,.3,.1])
-print(gray_level)
+    return d
+
+
+# In[20]:
+
+
 def convert_rgb_to_gray_level(im_1):
     m=im_1.shape[0]
     n=im_1.shape[1]
@@ -23,28 +27,21 @@ def convert_rgb_to_gray_level(im_1):
             im_2[i,j]=get_distance(im_1[i,j,:])
             
     return im_2
-im_2=convert_rgb_to_gray_level(img)
-type(im_2)
-plt.imshow(im_2,cmap="gray")
-plt.show()
-def convert_gray_level_to_BW(image_gray_level):
-    m=image_gray_level.shape[0]
-    n=image_gray_level.shape[1]
-    im_bw=np.zeros((m,n))
-    for i in range(m):
-        for j in range(n):
-            if image_gray_level[i,j]>100:
-                im_bw[i,j]=1
-            else:
-                im_bw[i,j]=0
-    return im_bw
-im_3=convert_gray_level_to_BW(im_2)
+
+img_3=convert_rgb_to_gray_level(img)
+
+
+# In[21]:
+
+
 plt.subplot(1,3,1),plt.imshow(img)
-plt.subplot(1,3,2),plt.imshow(im_2,cmap="gray")
-plt.subplot(1,3,3),plt.imshow(im_3,cmap="gray")
+plt.subplot(1,3,2),plt.imshow(img_3,cmap="gray")
 
 
-def pixel_compenent(img_3):    
+# In[24]:
+
+
+def pixel_compenent(img):    
     m=img.shape[0]   # black 0  white 1 
     n=img.shape[1]
     external=0
@@ -67,3 +64,10 @@ def pixel_compenent(img_3):
                 
     print(external)
     print(internal)
+    print((external-internal)/4)
+
+
+# In[25]:
+
+
+pixel_compenent(img_3)
